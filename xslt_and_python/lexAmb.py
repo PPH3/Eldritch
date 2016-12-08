a2b29dc2eeb1352601f2ffcd3f5ca51de584d1de
ebeshero <ebbondar@gmail.com>
2016-12-08 14:56:56 -0500

    updated python to output each full synset and definition with each word

--- xslt_and_python/lexAmb.py
+++ xslt_and_python/lexAmb.py
@@ -4,8 +4,12 @@ with open('wordList.txt', 'r') as infile: # open the plain text file that contai
 with open('synset_counts.xml', 'w') as outfile: # open a file to hold the XML output
     outfile.write('<xml>') # create a start tag for the root element in the output XML file
     for word in wordlist: # create output for each word
+        synsets = wn.synsets(word) #synset list
         synset_count = len(wn.synsets(word)) # for each word, count the number of synsets to which it belongs
-        outfile.write('<word><w>' + word + '</w><synsetCount>' + str(synset_count) + '</synsetCount></word>') # write it out
+        outfile.write('<word><w>' + word + '</w><synsetCount>' + str(synset_count) + '</synsetCount>')
+        for synset in synsets:
+            outfile.write('<synset><synId>' + str(synset) + '</synId><wnDef>' + synset.definition() + '</wnDef></synset>')
+        outfile.write('</word>') # write it out
     outfile.write('</xml>') # create the end tag for the root element
     
     
